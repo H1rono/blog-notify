caae22187c3934b470e8513341cff39cf591bf1b
H1rono <hronok66@gmail.com>
2023-08-20 08:24:31 +0900

    :recycle: Reduce direct use of GAS.Properties

--- main.ts
+++ main.ts
@@ -1,14 +1,48 @@
 const WRITER_REGEXP = /@[a-zA-Z0-9_-]+/g
 
+type CrowiInfo = {
+    host: string
+    pagePath: string
+    token: string
+}
+
+type traQInfo = {
+    channelId: string
+    logChannelId: string
+    webhookId: string
+    webhookSecret: string
+}
+
+type BlogRelayInfo = {
+    startDate: string
+}
+
 type InitResult = {
-    props: GoogleAppsScript.Properties.Properties
+    crowi: CrowiInfo
+    traQ: traQInfo
+    blogRelay: BlogRelayInfo
     noticeMessage: string
 }
 
-function init(): InitResult {
+function init(): InitResult | null {
     const props = PropertiesService.getScriptProperties()
-    const crowiHost = props.getProperty("CROWI_HOST") ?? "crowi.example.com"
-    const crowiPath = props.getProperty("CROWI_PAGE_PATH") ?? "/path/to/page"
+    const crowiHost = props.getProperty("CROWI_HOST")
+    const crowiPath = props.getProperty("CROWI_PAGE_PATH")
+    const crowiToken = props.getProperty("CROWI_ACCESS_TOKEN")
+    if (crowiHost === null || crowiPath === null || crowiToken === null) {
+        return null
+    }
+    const traQChannelId = props.getProperty("TRAQ_CHANNEL_ID")
+    const traQLogChannelId = props.getProperty("TRAQ_LOG_CHANNEL_ID")
+    const traQWebhookId = props.getProperty("WEBHOOK_ID")
+    const traQWebhookSecret = props.getProperty("WEBHOOK_SECRET")
+    if (traQChannelId === null || traQLogChannelId === null || traQWebhookId === null || traQWebhookSecret === null) {
+        return null
+    }
+    const blogRelayStartDate = props.getProperty("START_DATE")
+    if (blogRelayStartDate === null) {
+        return null
+    }
     const url = `https://${crowiHost}${crowiPath}`
     const noticeMessage = `
 ## 注意事項
@@ -18,33 +52,48 @@ function init(): InitResult {
 - **post imageを設定して**ください
 - わからないことがあれば気軽に #event/welcome/blog/buri まで
 - 詳細は ${url}`
-    return { props, noticeMessage }
+    return {
+        crowi: {
+            host: crowiHost,
+            pagePath: crowiPath,
+            token: crowiToken,
+        },
+        traQ: {
+            channelId: traQChannelId,
+            logChannelId: traQLogChannelId,
+            webhookId: traQWebhookId,
+            webhookSecret: traQWebhookSecret,
+        },
+        blogRelay: {
+            startDate: blogRelayStartDate,
+        },
+        noticeMessage,
+    }
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function main(): void {
-    const { props, noticeMessage } = init()
-    const pageBody = getCrowiPageBody(props)
+    const v = init()
+    if (v === null) {
+        Logger.log("init failed")
+        return
+    }
+    const { crowi, traQ, blogRelay, noticeMessage } = v
+    const pageBody = getCrowiPageBody(crowi)
     const schedules = extractSchedule(pageBody)
-    const dateDiff = calcDateDiff(props)
+    const dateDiff = calcDateDiff(blogRelay)
     const messageHead = dateDiff < 0 ? getBeforeMessage(-dateDiff) : getDuringMessage(dateDiff, schedules)
-    const res = postMessage(props, messageHead + noticeMessage, false)
+    const res = postMessage(traQ, messageHead + noticeMessage, false)
     Logger.log(res?.getResponseCode())
     // Logger.log(messageHead + noticeMessage)
     const logMessage = extractScheduleStr(pageBody)
-    const res2 = postMessage(props, logMessage, true)
+    const res2 = postMessage(traQ, logMessage, true)
     Logger.log(res2?.getResponseCode())
     // Logger.log(logMessage)
 }
 
-function getCrowiPageBody(props: GoogleAppsScript.Properties.Properties): string {
-    const host = props.getProperty("CROWI_HOST")
-    const token = props.getProperty("CROWI_ACCESS_TOKEN")
-    const path = props.getProperty("CROWI_PAGE_PATH")
-    if (host === null || token === null || path === null) {
-        return ""
-    }
-    const encodedPath = encodeURI(path)
+function getCrowiPageBody({ host, pagePath, token }: CrowiInfo): string {
+    const encodedPath = encodeURI(pagePath)
     const url = `https://${host}/_api/pages.get?access_token=${token}&path=${encodedPath}`
     const res = UrlFetchApp.fetch(url)
     const payload = JSON.parse(res.getContentText())
@@ -62,18 +111,11 @@ function hmacSha1(key: string, message: string): string {
 }
 
 function postMessage(
-    props: GoogleAppsScript.Properties.Properties,
+    { channelId, logChannelId, webhookId, webhookSecret }: traQInfo,
     content: string,
     log: boolean,
 ): GoogleAppsScript.URL_Fetch.HTTPResponse | null {
-    const webhookSecret = props.getProperty("WEBHOOK_SECRET")
-    const signature = webhookSecret != null ? hmacSha1(webhookSecret, content) : ""
-    const channelId = props.getProperty("TRAQ_CHANNEL_ID")
-    const logChannelId = props.getProperty("TRAQ_LOG_CHANNEL_ID")
-    const webhookId = props.getProperty("WEBHOOK_ID")
-    if (signature === null || channelId === null || logChannelId === null || webhookId === null) {
-        return null
-    }
+    const signature = hmacSha1(webhookSecret, content)
     const params: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
         method: "post",
         contentType: "text/plain; charset=utf-8",
@@ -152,12 +194,8 @@ function extractSchedule(pageBody: string): Schedule[] {
 
 // START_DATEとの差分を取得する
 // now - date
-function calcDateDiff(props: GoogleAppsScript.Properties.Properties): number {
-    const d = props.getProperty("START_DATE")
-    if (d === null) {
-        return 0
-    }
-    const date = new Date()
+function calcDateDiff({ startDate }: BlogRelayInfo): number {
+    const date = new Date(startDate)
     const dateUtcTime = date.getTime() + date.getTimezoneOffset() * 60 * 1000
     const now = new Date()
     const nowUtcTime = now.getTime() + now.getTimezoneOffset() * 60 * 1000
